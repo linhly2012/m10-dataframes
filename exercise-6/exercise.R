1ce224dd87a6b07f3c842266c66006f71844d727
Linh Ly <linhly@D-69-91-132-31.dhcp4.washington.edu>
2017-04-12 15:34:15 -0700

    .

--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -1,6 +1,7 @@
 # Exercise 7: Husky Football 2015 Season
 # Read in the Husky Football 2015 game data into a variable called `husky.games.2015`
 
+
 # Create a vector of the teams that the Huskies played against during that season
 # Call this vector `not.huskies`. You'll need to convert this column to a vector
 
